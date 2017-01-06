144752edba997242e5b278470b281f105597bcd2
HAlexTM <halexercraft@gmail.com>
2017-01-06 19:46:00 +0100

    Sitemap 1 hour cache

--- pages/sitemap.php
+++ pages/sitemap.php
@@ -1,22 +1,36 @@
 <?php
 header("Content-type: text/xml");
-echo "<?xml version='1.0' encoding='UTF-8'?>";
 ?>
+<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <?php
-include '../session.php';
-$sql = query("/*qc=on*/ SELECT Members.Nick, Posts.id FROM Posts JOIN Members ON Members.id = Posts.user");
-while ($string = mysqli_fetch_array($sql)){?>
+$file = "./sitemap.xml";
+if (!file_exists($file) || filemtime($file) < time() - 3600) {
+    $content;
+    ob_start();
+    $sql = query("/*qc=on*/ SELECT Members.Nick, Posts.id FROM Posts JOIN Members ON Members.id = Posts.user");
+    while ($string = mysqli_fetch_array($sql)) {
+?>
     <url>
         <loc>https://tras.pw/post/<?php echo $string['Nick']."/".$string['id']; ?></loc>
         <changefreq>weekly</changefreq>
     </url>
-<?php }
-$sql = query("/*qc=on*/ SELECT Nick FROM Members");
-while ($string = mysqli_fetch_array($sql)){?>
+<?php
+    }
+    $sql = query("/*qc=on*/ SELECT Nick FROM Members");
+    while ($string = mysqli_fetch_array($sql)) {
+?>
     <url>
         <loc>https://tras.pw/user/<?php echo $string['Nick']; ?></loc>
         <changefreq>weekly</changefreq>
     </url>
-<?php } ?>
+<?php
+    }
+    $content = ob_get_clean();
+    file_put_contents($file, $content);
+    echo $content;
+} else {
+    echo file_get_contents($file);
+}
+?>
 </urlset>
