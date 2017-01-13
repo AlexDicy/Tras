6937fde29d1974757d8bcd594700bf27f76982ea
HAlexTM <halexercraft@gmail.com>
2017-01-13 17:59:10 +0100

    fix notification on like

--- pages/new.php
+++ pages/new.php
@@ -4,7 +4,6 @@ if (isset($_POST['text']) && isLoggedIn()) {
     $user = $_SESSION['info']['id'];
     $sql = query("INSERT INTO Posts (user, content) VALUES ('$user', '$text')");
     if ($sql) {
-        //newNotification($user, 0, 0, 4, "Your new post: ".$_POST['text']);
 ?>
 {"CODE": 200}
 <?php
--- pages/opinionmanager.php
+++ pages/opinionmanager.php
@@ -4,14 +4,16 @@ if (isset($_POST['id']) && isLoggedIn()) {
     $type = escape($_POST['type']);
     $user = $_SESSION['info']['id'];
     $opinion = ($type == 0) ? 2 : 1;
-    $postcontent = mysqli_fetch_array(query("SELECT SUBSTRING(Posts.content, 1, 30) FROM Posts WHERE Posts.id = '$post' LIMIT 1"))[0];
+    $postinfo = mysqli_fetch_array(query("SELECT SUBSTRING(Posts.content, 1, 30), user FROM Posts WHERE Posts.id = '$post' LIMIT 1"));
+    $postcontent = $postinfo[0];
+    $postuser = $postinfo[1];
     if (isset($_POST['add'])) {
         $sql = query("INSERT IGNORE INTO Opinions(result,user,post,type) VALUES ((1000000000*'$user')+'$post','$user','$post','$type')");
         //Do not notify for dislikes atm
-        if ($opinion == 1) newNotification($friend, $user, $post, $opinion, $postcontent."...");
+        if ($opinion == 1) newNotification($postuser, $user, $post, $opinion, $postcontent."...");
     } else {
         $sql = query("DELETE FROM Opinions WHERE result = (1000000000*'$user')+'$post'");
-        if ($opinion == 1) deleteNotification($friend, $user, $post, $opinion, $postcontent."...");
+        if ($opinion == 1) deleteNotification($postuser, $user, $post, $opinion, $postcontent."...");
     }
     if ($sql) {
 ?>
