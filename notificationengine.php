84f5993c8a84f76383e662d6847c3dcc4e32235a
HAlexTM <halexercraft@gmail.com>
2017-01-17 12:15:43 +0100

    Fix notifications for not logged in users

--- notificationengine.php
+++ notificationengine.php
@@ -1,36 +1,38 @@
 <?php
 function getNotifications($num) {
-    $userid = $_SESSION['info']['id'];
-    $where = "WHERE user = $userid AND Notifications.from <> $userid AND Notifications.hide = 0";
-    $query = query("SELECT Notifications.id, Notifications.user, Notifications.from, Notifications.where, Notifications.type, Notifications.when, Notifications.content, Notifications.viewed, Members.Avatar, Members.Nick FROM Notifications LEFT JOIN Members ON Members.id = Notifications.from $where ORDER BY id DESC LIMIT $num");
-    $count = mysqli_num_rows(query("SELECT id FROM Notifications $where AND viewed = 0 AND hide = 0 LIMIT 100"));
     $array = array();
-    $array['count'] = ($count == 0 ? "":($count == 100 ? "99+":$count));
-    while ($n = mysqli_fetch_assoc($query)) {
-        switch ($n['type']) {
-            case 1:
-                $n['title'] = $n['Nick']." likes your post";
-                $n['link'] = "/post/".$_SESSION['info']['Nick']."/".$n['where'];
-                break;
-            case 2:
-                $n['title'] = $n['Nick']." dislikes your post";
-                $n['link'] = "/post/".$_SESSION['info']['Nick']."/".$n['where'];
-                break;
-            case 3:
-                $n['title'] = $n['Nick']." replied to your post";
-                $n['link'] = "/post/".$_SESSION['info']['Nick']."/".$n['where'];
-                break;
-            case 4:
-                $n['title'] = "Tras alert";
-                $n['link'] = "/alert/".$n['id'];
-                $n['Avatar'] = "https://tras.pw/images/logo-min.png";
-                break;
-            case 5:
-                $n['title'] = $n['Nick']." has added you as friend";
-                $n['link'] = "/user/".$n['Nick'];
-                break;
+    if (isLoggedIn()) {
+        $userid = $_SESSION['info']['id'];
+        $where = "WHERE user = $userid AND Notifications.from <> $userid AND Notifications.hide = 0";
+        $query = query("SELECT Notifications.id, Notifications.user, Notifications.from, Notifications.where, Notifications.type, Notifications.when, Notifications.content, Notifications.viewed, Members.Avatar, Members.Nick FROM Notifications LEFT JOIN Members ON Members.id = Notifications.from     $where ORDER BY id DESC LIMIT $num");
+        $count = mysqli_num_rows(query("SELECT id FROM Notifications $where AND viewed = 0 AND hide = 0 LIMIT 100"));
+        $array['count'] = ($count == 0 ? "":($count == 100 ? "99+":$count));
+        while ($n = mysqli_fetch_assoc($query)) {
+            switch ($n['type']) {
+                case 1:
+                    $n['title'] = $n['Nick']." likes your post";
+                    $n['link'] = "/post/".$_SESSION['info']['Nick']."/".$n['where'];
+                    break;
+                case 2:
+                    $n['title'] = $n['Nick']." dislikes your post";
+                    $n['link'] = "/post/".$_SESSION['info']['Nick']."/".$n['where'];
+                    break;
+                case 3:
+                    $n['title'] = $n['Nick']." replied to your post";
+                    $n['link'] = "/post/".$_SESSION['info']['Nick']."/".$n['where'];
+                    break;
+                case 4:
+                    $n['title'] = "Tras alert";
+                    $n['link'] = "/alert/".$n['id'];
+                    $n['Avatar'] = "https://tras.pw/images/logo-min.png";
+                    break;
+                case 5:
+                    $n['title'] = $n['Nick']." has added you as friend";
+                    $n['link'] = "/user/".$n['Nick'];
+                    break;
+            }
+            $array[] = $n;
         }
-        $array[] = $n;
     }
     return $array;
 }
