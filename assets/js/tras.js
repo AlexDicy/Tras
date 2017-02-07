9757a0e4284caff672dd1ae1dd58c86d32b180c5
HAlex <halexercraft@gmail.com>
2017-02-07 16:22:13 +0100

    what? my latest commit didn't upload

--- assets/js/tras.js
+++ assets/js/tras.js
@@ -4,6 +4,66 @@ var css = "text-shadow: -1px -1px hsl(0,100%,50%), 1px 1px hsl(5.4, 100%, 50%), 
 console.log("%cWarning: %s", css, 'Don\'t use this!!');
 $(function() {
 
+if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register('/assets/js/service-worker.js')  
+    .then(initialiseState);  
+} else {  
+    console.warn('Service workers aren\'t supported in this browser.');  
+}
+
+function initialiseState() {  
+  // Are Notifications supported in the service worker?  
+  if (!('showNotification' in ServiceWorkerRegistration.prototype)) {  
+    console.warn('Notifications aren\'t supported.');  
+    return;  
+  }
+
+  // Check the current Notification permission.  
+  // If its denied, it's a permanent block until the  
+  // user changes the permission  
+  if (Notification.permission === 'denied') {  
+    console.warn('The user has blocked notifications.');  
+    return;  
+  }
+
+  // Check if push messaging is supported  
+  if (!('PushManager' in window)) {  
+    console.warn('Push messaging isn\'t supported.');  
+    return;  
+  }
+
+  // We need the service worker registration to check for a subscription  
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
+    // Do we already have a push message subscription?  
+    serviceWorkerRegistration.pushManager.getSubscription()  
+      .then(function(subscription) {  
+        // Enable any UI which subscribes / unsubscribes from  
+        // push messages.  
+    
+        //var pushButton = document.querySelector('.js-push-button');  
+        //pushButton.disabled = false;
+
+        if (!subscription) {  
+          // We aren't subscribed to push, so set UI  
+          // to allow the user to enable push  
+          return;  
+        }
+
+        // Keep your server in sync with the latest subscriptionId
+        sendSubscriptionToServer(subscription);
+
+        // Set your UI to show they have subscribed for  
+        // push messages  
+        
+        //pushButton.textContent = 'Disable Push Messages';  
+        //isPushEnabled = true;  
+      })  
+      .catch(function(err) {  
+        console.warn('Error during getSubscription()', err);  
+      });  
+  });  
+}
+
 $(".autosize").autosize({append: "\n"});
 $("#new-post-button").on("click", function(){
     sAlert("#new-post-error", false);
--- nodejs/index.js
+++ nodejs/index.js
@@ -13,6 +13,7 @@ http.listen(3000, "localhost", function(){
     console.log("Listening on 3000");
 });
 
+
 io.on('connection',function(socket){
     socket.on('commend added',function(data){
 
