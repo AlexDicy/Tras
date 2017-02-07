fea9754629d8ea77df4e97c00169ab3d4960cfb9
HAlex <halexercraft@gmail.com>
2017-02-07 20:47:24 +0100

    add 512px favicon

--- template/head.php
+++ template/head.php
@@ -19,7 +19,8 @@
     <meta property="fb:app_id" content="198347767255916">
     <meta name="theme-color" content="#43a8eb">
     <title><?php echo getTitle(Shared::get("name"), Shared::get("link"), Shared::get("path"), Shared::get("infoNick")) ?></title>
-    <link id="favicon" rel="icon" type="image/png" href="/images/logo-128.png" sizes="45x45">
+    <link id="favicon" rel="icon" type="image/png" href="/images/logo-128.png" sizes="128x128">
+    <link id="favicon" rel="icon" type="image/png" href="/images/logo-min-512.png" sizes="512x512">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
     <link type="text/css" rel="stylesheet" href="//<?php echo Shared::get("host") ?>/assets/styles/tras.css?v148">
