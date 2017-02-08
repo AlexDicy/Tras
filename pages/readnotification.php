bbca47b42ad30e43a5ee20b89e5103ba51b0cbed
HAlex <halexercraft@gmail.com>
2017-02-08 08:07:42 +0100

    fix double slash

--- pages/readnotification.php
+++ pages/readnotification.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['link']) && isLoggedIn()) {
     $where = escape($_GET['where']);
-    header("Location: https://tras.pw/" . $_GET['link']);
+    header("Location: https://tras.pw" . $_GET['link']);
     $user = $_SESSION['info']['id'];
     $sql = query("UPDATE `Notifications` SET `viewed` = 1 WHERE `user` = '$user' AND `where` = '$where'");
 }
