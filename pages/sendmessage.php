6bdc37d59174d931b0f066adcf72e8074cd3283b
HAlex <halexercraft@gmail.com>
2017-03-26 19:37:09 +0200

    only send chat message if user is part of a chat

--- pages/sendmessage.php
+++ pages/sendmessage.php
@@ -3,18 +3,24 @@ if (isset($_POST['content']) && isset($_POST['chat_id'])) {
     $text = escape(base64_encode(htmlentities($_POST['content'])));
     $chatId = escape($_POST['chat_id']);
     $user = $_SESSION['info']['id'];
-    $sql = query("INSERT INTO Messages (user, chat_id, content) VALUES ('$user', '$chatId', '$text')");
-    $users = query("SELECT user FROM Chats WHERE chat_id = '$chatId' AND user <> '$user'");
-    if ($sql && $_POST['content'] != "") {
-        echo "{\"CODE\": 700}";
-        if ($users) {
-            while ($info = mysqli_fetch_array($users)) {
-                query("UPDATE Chats SET `read` = 0 WHERE chat_id = '$chatId' AND user = '". $info['user'] ."'");
-                newNotification($info['user'], $user, $chatId, 6, substr(base64_decode($text), 0, 50));
+    $users = query("SELECT user FROM Chats WHERE chat_id = '$chatId'");
+    if (in_array($user, $users)) {
+        $sql = query("INSERT INTO Messages (user, chat_id, content) VALUES ('$user', '$chatId', '$text')");
+        if ($sql && $_POST['content'] != "") {
+            echo "{\"CODE\": 700}";
+            if ($users) {
+                while ($info = mysqli_fetch_array($users)) {
+                    if ($info['user'] != $user) {
+                        query("UPDATE Chats SET `read` = 0 WHERE chat_id = '$chatId' AND user = '". $info['user'] ."'");
+                        newNotification($info['user'], $user, $chatId, 6, substr(base64_decode($text), 0, 50));
+                    }
+                }
             }
+        } else {
+            echo "{\"CODE\": 666}";
         }
     } else {
-        echo "{\"CODE\": 666}";
+            echo "{\"CODE\": 666}";
     }
 }
 ?>
