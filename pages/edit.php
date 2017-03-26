70027a0aed023c5a152e594a40bd0901117328aa
HAlex <halexercraft@gmail.com>
2017-03-26 21:01:46 +0200

    show a warning if you're trying to edit not-your-post

--- pages/edit.php
+++ pages/edit.php
@@ -1,11 +1,12 @@
 <?php
 $id = escape(Shared::get("path")[1]);
 $userid = $_SESSION['info']['id'];
-$post = query("SELECT Posts.id, Posts.content, Members.Nick FROM Posts JOIN Members ON Members.id = Posts.user WHERE Posts.id = '$id' AND Posts.user = '$userid'");
+$post = query("SELECT Posts.id, Posts.content, Members.Nick, Members.id as user_id FROM Posts JOIN Members ON Members.id = Posts.user WHERE Posts.id = '$id' AND Posts.user = '$userid'");
 $info = mysqli_fetch_array($post);
 ?>
 <div class="alert alert-warning" style="display: none" id="edit-post-error"><strong>Error</strong> There was an error while editing, please try again.</div>
 <div class="post post-margin-bottom">
+	<?php if ($userid == $info['user_id']) { ?>
 	<div class="portlet">
 		<div class="portlet-title">
 			<div class="caption">
@@ -21,4 +22,7 @@ $info = mysqli_fetch_array($post);
 			</div>
 		</div>
 	</div>
+	<?php } else { ?>
+		<div class="alert alert-warning"><strong>Error</strong> This post isn't your.</div>
+	<?php } ?>
 </div>
