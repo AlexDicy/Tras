cf8567ec4ef3ea9ae4a8bbae617b68b915e744c3
maxbycode <maxbycode@gmail.com>
2017-03-30 16:52:23 -0600

    making pull more forceful

--- gitupdate.php
+++ gitupdate.php
@@ -1,5 +1,7 @@
 <?php
 echo '<pre>';
-echo shell_exec("git pull origin master 2>&1");
+echo shell_exec("git fetch origin master 2>&1");
+echo shell_exec("git reset --hard FETCH_HEAD");
+echo shell_exec("git clean -df");
 echo '</pre>';
 ?>
