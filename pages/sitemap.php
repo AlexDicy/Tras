95e51713b843e21da55cb8a94397d2b5c40fcfb1
HAlexTM <halexercraft@gmail.com>
2017-05-12 20:06:27 +0200

    Sitemap: only write users

--- pages/sitemap.php
+++ pages/sitemap.php
@@ -1,22 +1,13 @@
 <?php
 header("Content-type: text/xml");
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <?php
 $file = "./sitemap.xml";
 if (!file_exists($file) || filemtime($file) < time() - 3600) {
     $content;
     ob_start();
-    $sql = query("/*qc=on*/ SELECT Members.nick, Posts.id FROM Posts JOIN Members ON Members.id = Posts.user");
-    while ($string = mysqli_fetch_array($sql)) {
-?>
-    <url>
-        <loc>https://tras.pw/post/<?php echo $string['nick']."/".$string['id']; ?></loc>
-        <changefreq>weekly</changefreq>
-    </url>
-<?php
-    }
     $sql = query("/*qc=on*/ SELECT nick FROM Members");
     while ($string = mysqli_fetch_array($sql)) {
 ?>
