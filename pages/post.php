a62a939366b39b8d4cfb129f1da5cd7e02a82f7d
Lorenzo Lapucci <halexercraft@gmail.com>
2017-05-12 08:11:05 +0200

    Manage 404 errors with Posts

--- pages/post.php
+++ pages/post.php
@@ -1,6 +1,6 @@
 <?php
 $id = escape(Shared::get("path")[2]);
-$info = mysqli_fetch_array(Shared::get("get")['posts']->getPost($id));
+$info = Shared::get("content");
 if ($info) {
 ?>
 <div class="posts-col col-md-6">
