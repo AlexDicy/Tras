db40bcfe5bf21fc87571c8c76d85eca5231873ba
HAlexTM <halexercraft@gmail.com>
2017-05-15 17:42:35 +0200

    forgot to type "query()"

--- pages/search.php
+++ pages/search.php
@@ -1,10 +1,10 @@
 <?php
 if (isset($_GET['query'])){
     $query = escape($_GET['query']);
-    $sql = "SELECT 0 AS id, 0 AS content, Members.nick AS nick FROM Members WHERE Members.nick LIKE '%$query%' LIMIT 30
+    $sql = query("SELECT 0 AS id, 0 AS content, Members.nick AS nick FROM Members WHERE Members.nick LIKE '%$query%' LIMIT 30
                   UNION
-                  SELECT Posts.id, Members.nick, Posts.content FROM Posts JOIN Members ON Posts.user = Members.id WHERE content LIKE '%$query%' LIMIT 30
-    ";
+                  SELECT Posts.id, Posts.content, Members.nick FROM Posts JOIN Members ON Posts.user = Members.id WHERE Posts.content LIKE '%$query%' LIMIT 30
+    ");
     while ($info = mysqli_fetch_array($sql)) {
 ?>
         <a href="/<?= $info["id"] == 0 ? "user/".$info['nick'] : "post/".$info['nick']."/".$info['id'] ?>">
@@ -16,7 +16,7 @@ if (isset($_GET['query'])){
                     <div class="col-xs-8">
                         <div class="panel-body text-center">
                             <h4 class="mt0"><?= $info['nick'] ?></h4>
-                            <p class="mb0 text-muted"><?php if ($info["id"] == 0) echo Shared::removeFormatting($info['content']) ?></p>
+                            <p class="mb0 text-muted"><?php if ($info["id"] != 0) echo Shared::removeFormatting($info['content']) ?></p>
                         </div>
                     </div>
                 </div>
