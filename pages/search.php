d2a7da4e127569643d4367e34467fd69aa1b234e
HAlex <halexercraft@gmail.com>
2017-05-15 17:36:50 +0200

    merge search queries

--- pages/search.php
+++ pages/search.php
@@ -1,39 +1,22 @@
 <?php
 if (isset($_GET['query'])){
-    $name = escape($_GET['query']);
-    $sql = query("SELECT id, nick FROM Members WHERE nick LIKE '%" . $name .  "%' LIMIT 30");
-    while($info = mysqli_fetch_array($sql)){
+    $query = escape($_GET['query']);
+    $sql = "SELECT 0 AS id, 0 AS content, Members.nick AS nick FROM Members WHERE Members.nick LIKE '%$query%' LIMIT 30
+                  UNION
+                  SELECT Posts.id, Members.nick, Posts.content FROM Posts JOIN Members ON Posts.user = Members.id WHERE content LIKE '%$query%' LIMIT 30
+    ";
+    while ($info = mysqli_fetch_array($sql)) {
 ?>
-        <a href="/user/<?php echo $info['nick'] ?>">
-            <div id="panelPortlet4" class="panel panel-default b0">
-                <div class="row row-table row-flush">
-                    <div class="col-xs-4 bg-danger text-center">
-                        <em class="fa fa-user fa-2x"></em>
-                    </div>
-                    <div class="col-xs-8">
-                        <div class="panel-body text-center">
-                            <h4 class="mt0"><?php echo $info['nick'] ?></h4>
-                            <p class="mb0 text-muted"><?php echo $info['id'] ?></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </a>
-<?php
-    }
-$sql = query("SELECT Posts.id, Members.nick, Posts.content FROM Posts JOIN Members ON Posts.user = Members.id WHERE content LIKE '%" . $name .  "%' LIMIT 30");
-    while($info = mysqli_fetch_array($sql)){
-?>
-        <a href="/post/<?php echo $info['nick']."/".$info['id'] ?>">
+        <a href="/<?= $info["id"] == 0 ? "user/".$info['nick'] : "post/".$info['nick']."/".$info['id'] ?>">
             <div id="panelPortlet4" class="panel panel-default b0">
                 <div class="row row-table row-flush">
                     <div class="col-xs-4 bg-inverse text-center">
-                        <em class="fa fa-file-text fa-2x"></em>
+                        <em class="fa fa-<?= $info["id"] == 0 ? "user" : "file-text" ?> fa-2x"></em>
                     </div>
                     <div class="col-xs-8">
                         <div class="panel-body text-center">
                             <h4 class="mt0"><?= $info['nick'] ?></h4>
-                            <p class="mb0 text-muted"><?= Shared::removeFormatting($info['content']) ?></p>
+                            <p class="mb0 text-muted"><?php if ($info["id"] == 0) echo Shared::removeFormatting($info['content']) ?></p>
                         </div>
                     </div>
                 </div>
