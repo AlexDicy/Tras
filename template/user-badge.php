e6f6b2dc658ca3aa2ed81d3bd8f3cce5436e2c8c
HAlex <halexercraft@gmail.com>
2017-05-16 18:45:57 +0200

    fix user-badge.php

--- template/user-badge.php
+++ template/user-badge.php
@@ -3,9 +3,9 @@
         <div class="row row-table row-flush">
             <div class="sidebar-avatar text-center" style="background-image:url(<?php echo $info['avatar'] ?>);"></div>
             <div class="col-xs-8">
-                <div class="panel-body text-center">
+                <div class="panel-body">
                     <div class="pull-left" style="margin-left: 20px;position: absolute;margin-top: -8px;">
-                        <a href="/user/<?php echo $info['nick'] ?>/"<h4 class="mt0"><?php echo $info['nick'] ?></h4></a>
+                        <a href="/user/<?php echo $info['nick'] ?>/"><h5 class="mt0"><?php echo $info['nick'] ?></h5></a>
                         <p class="mb0 text-muted">Friends: <?php echo getFriendsCount($info['id']) ?></p>
                     </div>
                     <div class="pull-right">
